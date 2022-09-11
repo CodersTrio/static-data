78cba4172eb8324a615a0d6ad2ba6493abe51e16
Ashraful Islam <62855422+ash358241@users.noreply.github.com>
2022-09-11 18:51:43 +0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,142 @@
+export const dummyData = [
+    {
+        _id: 1,
+        name: 'Ashraf',
+        email: 'ashraf@gmail.com',
+        incoming: {
+            route: 'Route-1',
+            schedule: '10AM',
+            stoppage: 'Londoni road',
+        },
+        outgoing: {
+            route: 'Route-1',
+            schedule: '2PM'
+        }
+    },
+    {
+        _id: 2,
+        name: 'Ashfak',
+        email: 'ashfak@gmail.com',
+        incoming: {
+            route: 'Route-1',
+            schedule: '11AM',
+            stoppage: 'Lovely road',
+        },
+        outgoing: {
+            route: 'Route-2',
+            schedule: '3PM'
+        },
+    },
+    {
+        _id: 3,
+        name: 'Ahnaf',
+        email: 'ahnaf@gmail.com',
+        incoming: {
+            route: 'Route-2',
+            schedule: '9AM',
+            stoppage: 'Modina market',
+        },
+        outgoing: {
+            route: 'Route-2',
+            schedule: '2PM'
+        }
+    },
+    {
+        _id: 4,
+        name: 'Tahmid',
+        email: 'tahmid@gmail.com',
+        incoming: {
+            route: 'Route-3',
+            schedule: '10AM',
+            stoppage: 'Londoni road',
+        },
+        outgoing: {
+            route: 'Route-3',
+            schedule: '2PM'
+        }
+    },
+    {
+        _id: 5,
+        name: 'Ashab',
+        email: 'ashab@gmail.com',
+        incoming: {
+            route: 'Route-2',
+            schedule: '8AM',
+            stoppage: 'Modina market',
+        },
+        outgoing: {
+            route: 'Route-2',
+            schedule: '4PM'
+        }
+    },
+    {
+        _id: 6,
+        name: 'Hussen',
+        email: 'hussen@gmail.com',
+        incoming: {
+            route: 'Route-3',
+            schedule: '10AM',
+            stoppage: 'Nobab road',
+        },
+        outgoing: {
+            route: 'Route-3',
+            schedule: '4PM'
+        }
+    },
+    {
+        _id: 7,
+        name: 'Islam',
+        email: 'islam@gmail.com',
+        incoming: {
+            route: 'Route-1',
+            schedule: '8AM',
+            stoppage: 'Mazar road',
+        },
+        outgoing: {
+            route: 'Route-1',
+            schedule: '2PM'
+        }
+    },
+    {
+        _id: 8,
+        name: 'Jalal',
+        email: 'jalal@gmail.com',
+        incoming: {
+            route: 'Route-2',
+            schedule: '8AM',
+            stoppage: 'Rikabi bazar',
+        },
+        outgoing: {
+            route: 'Route-2',
+            schedule: '4PM'
+        }
+    },
+    {
+        _id: 9,
+        name: 'Ekram',
+        email: 'ekram@gmail.com',
+        incoming: {
+            route: 'Route-1',
+            schedule: '10AM',
+            stoppage: 'Mula road',
+        },
+        outgoing: {
+            route: 'Route-1',
+            schedule: '4PM'
+        }
+    },
+    {
+        _id: 10,
+        name: 'Modon',
+        email: 'modon@gmail.com',
+        incoming: {
+            route: 'Route-4',
+            schedule: '8AM',
+            stoppage: 'Londoni road',
+        },
+        outgoing: {
+            route: 'Route-4',
+            schedule: '5PM'
+        }
+    }
+]
